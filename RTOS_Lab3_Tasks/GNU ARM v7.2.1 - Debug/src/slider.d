6011430662967a02bc1318b0de64c1142d015ac7
christensenkj <christensenkarston@gmail.com>
2021-02-08 21:08:00 -0700

    initial commit

---
+++ RTOS_Lab3_Tasks/GNU ARM v7.2.1 - Debug/src/slider.d
@@ -0,0 +1,9 @@
+src/slider.o: ../src/slider.c ../src/slider.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.7\hardware\kit\common\drivers\capsense.h \
+ c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.7\hardware\kit\slstk3402a_efm32pg12\config\capsenseconfig.h
+
+../src/slider.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.7\hardware\kit\common\drivers\capsense.h:
+
+c:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.7\hardware\kit\slstk3402a_efm32pg12\config\capsenseconfig.h:
